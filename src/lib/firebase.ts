cd0bf29293e969a8127887bb45415685294904f3
MD MUKIT HASAN <mdmukithasan429@gmail.com>
2025-11-23 15:06:36 +0000

    firebase deploy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,8 +2,7 @@ import { initializeApp, getApp, getApps, FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// The environment variable isn't being picked up reliably in the dev environment.
-// For this prototype, we'll hardcode the config to ensure connectivity.
+// Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCLDIX2Zhwedm1yDH4ErHPWh5LrB6W8Xxw",
   authDomain: "studio-679351035-58c98.firebaseapp.com",
@@ -12,6 +11,7 @@ const firebaseConfig: FirebaseOptions = {
   messagingSenderId: "1000712197366",
   appId: "1:1000712197366:web:8b5983ef2cf53a5208c8d7"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
